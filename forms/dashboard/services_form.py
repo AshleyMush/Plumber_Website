54e8242daa79cb98286db9dc75de9a459f88728c
Ashley <tanaka.mush@gmail.com>
2025-01-06 08:59:24 +0000

    added other services boostrap and font awesome icon section to the dashboard

--- forms/dashboard/services_form.py
+++ forms/dashboard/services_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, FileField
-from wtforms.validators import DataRequired, URL, Optional
+from wtforms.validators import DataRequired, URL, Optional, Regexp
 from flask_wtf.file import FileAllowed, FileRequired
 
 from flask_ckeditor import CKEditorField
@@ -11,7 +11,16 @@ from flask_ckeditor import CKEditorField
 
 class ServicesForm(FlaskForm):
     name =StringField('Name of the service you provide', validators=[DataRequired()])
-    icon_class = StringField('Icon Class (Bootstrap or Font Awesome)', validators=[Optional()])
+    icon_class = StringField(
+        'Icon Class (Bootstrap or Font Awesome)',
+        validators=[
+            Optional(),
+            Regexp(
+                r'^(fa-[a-z0-9\- ]+|bi-[a-z0-9\- ]+)$',
+                message="Enter a valid Font Awesome (e.g., fa-solid fa-bath) or Bootstrap (e.g., bi-bootstrap-icon) class."
+            )
+        ]
+    )
 
     main_image_path: FileField = FileField(
         'Upload  Service Page and Home Page Main Image',
